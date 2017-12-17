448c8d05c4630cc3aed2dcb9f755b1380af2ab17
joma74 <dev-mails@gmx.net>
2017-12-17 23:23:06 +0100

    Vue component without Decorator

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>{{message}}</div>
+  <div @click="onClick">{{message}}</div>
 </template>
 
 <script>
 import Vue from "vue";
-import Component from "vue-class-component"
+import Component from "vue-class-component";
 
-@Component({})
-export default class App extends Vue {
-  message = "hello from class"
-}
+export default Component({})(
+  class App extends Vue {
+    message = "hello from class";
+
+    onClick(){
+      this.message = "goodbye from class" 
+    }
+  }
+);
 </script>
