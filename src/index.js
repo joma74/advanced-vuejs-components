7586ed0a2a4f7dc1ca2fe284923f7720848441ee
joma74 <dev-mails@gmx.net>
2017-12-20 01:34:46 +0100

    Chore formatting

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ new Vue({
     el: "#app",
     render: h => (
         <App>
-            <h2 slot="header" class="text-orange">Header</h2>
-            <h3 slot="footer" class="text-green">Footer</h3>
+            <h2 slot="header" class="text-orange">
+                Header
+            </h2>
+            <h3 slot="footer" class="text-green">
+                Footer
+            </h3>
         </App>
     )
 });
