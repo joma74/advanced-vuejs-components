8481b57ead809bd29d83637aa2549f11d2badb67
joma74 <dev-mails@gmx.net>
2017-12-21 23:57:22 +0100

    vue-slots-props completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <Layout>
-    <h1 slot="content" class="flex-grow" @click="onClick">Content goes here "{{message}}"!</h1>
-  </Layout>
+  <Settings>
+    <Layout slot-scope="{header, footer}">
+      <h1 slot="header" class="bg-blue text-white p-2">{{header}}</h1>
+      <h2 slot="footer" class="bg-grey-light text-blue text-center p-2">{{footer}}</h2>
+    </Layout>
+  </Settings>
 </template>
 
 <script>
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import Layout from "./Layout.vue";
+import Settings from "./Settings.vue";
 
 @Component({
   components: {
-    Layout
+    Layout,
+    Settings
   }
 })
 export default class App extends Vue {
