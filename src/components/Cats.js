d089df4a83488f6ecf26330178d292be55a600a1
joma74 <dev-mails@gmx.net>
2017-12-22 20:43:03 +0100

    Formatted Cats

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 
-const Cats = Vue.extend({
-    functional: true,
-    render: (h, {props}) => (<div>{
-      props.names.map(name => `https://robohash.org/${name}?set=set${props.num}`).
-      map(url => <img src={url} />)
-    }</div>),
-  });
+const CatsList = Vue.extend({
+      functional: true,
+      render: (h, { props }) => ( 
+        <div> {
+          props.names
+          .map( name => `https://robohash.org/${name}?set=set${props.num}`)
+          .map( url => <img src = {url}/>)
+          } 
+        </div>
+      ),
+});
 
-export default Cats;
+export default CatsList;
