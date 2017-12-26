03f7dc93b512190a2e4dc948eb40ba1ae79625c8
joma74 <dev-mails@gmx.net>
2017-12-26 01:59:38 +0100

    magic usage of props

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,5 +1,36 @@
 import Vue from "vue";
 
+const Selected = Vue.component("Selected",{
+  props: {
+    render: {
+      default: h => null
+    }
+  },
+  data() {
+    return {
+      selected: 0
+    }
+  },
+  methods: {
+    /**
+     * @param {number} value 
+     */
+    select(value) {
+      this.selected = value
+    }
+  },
+  render() {
+    return this.$props.render({
+      selected: this.selected,
+      select: this.select
+    })
+  },
+  computed: {
+    withoutthatinferencebreaks(){
+    }
+  }
+});
+
 const CatsList = Vue.extend({
   functional: true,
   render: (h, { props }) => {
@@ -7,8 +38,66 @@ const CatsList = Vue.extend({
       return <div>{
       propsNames
         .map(name => `https://robohash.org/${name}?set=set${props.num}`)
-        .map(url => <img src={url} />)
+        .map((url, index) => {
+          return (
+          <img 
+            class={props.selected == index ? "bg-blue" : ""}
+            onClick={event => props.select(index)}
+            src={url}
+          />)
+        })
       } </div>
+  },
+  props: {
+    names: {
+      type: Array,
+      required: true
+    },
+    num: {
+      type: Number,
+      required: true
+    },
+    selected: {
+      type: Number,
+      required: true
+    },
+    select: {
+      type: Function,
+      required: true
+    }
+  }
+});
+
+export default Vue.extend({
+  functional: true,
+  render: (h, { props }) => {
+    return (
+      <div>
+        <Selected render={
+          ({ // what should i say - arguments must be outside of render context
+             // these bunnies are bound to the value inside Selected' render function
+            selected, select
+          }) => {
+            return (
+              <div>
+                <input
+                  class="py-2 px-3 text-4x1"
+                  type="number"
+                  onChange={event => select(event.target.value)} // whenever triggered, store value inside Selected
+                  value={selected}
+                />
+                <CatsList 
+                  names={props.names}
+                  num={props.num}
+                  selected={Number.parseInt(selected)} // prop of CatsList
+                  select={select} // prop of CatsList
+                />
+              </div>
+            )
+          }
+        }/>
+      </div>
+    )
   },
   props: {
     names: {
@@ -20,6 +109,4 @@ const CatsList = Vue.extend({
       required: true
     }
   }
-});
-
-export default CatsList;
+})
