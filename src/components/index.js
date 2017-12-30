53656866e9352b5ed95d2bc4c80b3119f8504f09
joma74 <dev-mails@gmx.net>
2017-12-30 02:54:05 +0100

    chore(style) formatting with prettier

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,2 @@
-export {
-    default as Footer
-}
-from "./Footer.vue"
-export {
-    default as Header
-}
-from "./Header.vue"
+export { default as Footer } from "./Footer.vue";
+export { default as Header } from "./Header.vue";
